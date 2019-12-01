ac56b451d2682b3ca0c429066f57d5d43d62da98
wangchang327 <wchang@pku.edu.cn>
2019-12-02 01:05:48 +0800

    slight changes (mnist)

--- mnist-wangchang/train.py
+++ mnist-wangchang/train.py
@@ -7,7 +7,7 @@ import time
 import multiprocessing
 import numpy as np
 
-NEPOCH = 2
+NEPOCH = 1000
 batch_size = 50
 train_data = load_mnist_data()
 train_data = np.reshape(train_data, (-1, batch_size, 28, 28, 1))
