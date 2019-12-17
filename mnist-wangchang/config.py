0f8a973b8b9e60e286b9f4666964cf93dc876b47
wangchang327 <wchang@pku.edu.cn>
2019-12-17 10:45:11 +0800

    rename (mnist)

--- mnist-wangchang/config.py
+++
@@ -1,9 +0,0 @@
-class FLAGS(object):
-    def __init__(self):
-        self.n_epoch = 100
-        self.D_learning_rate = 2e-4
-        self.G_learning_rate = 1e-3
-        self.batch_size = 64
-
-
-flags = FLAGS()
